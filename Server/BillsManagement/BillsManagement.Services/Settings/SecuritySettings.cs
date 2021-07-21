6b94f9a33357de171c2e6b504affd2260d901397
Georgi Cholakov <cholakovge@gmail.com>
2021-07-21 22:17:59 +0200

    settings moved to service layer

---
+++ Server/BillsManagement/BillsManagement.Services/Settings/SecuritySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BillsManagement.Services
+{
+    public class SecuritySettings
+    {
+        public string JWT_Secret { get; set; }
+
+        public string Client_URL { get; set; }
+    }
+}
